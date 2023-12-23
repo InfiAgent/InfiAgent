1e93cb40c990b3be34aec2231c7551eb8e273c89
huxueyu <huxueyu@zju.edu.cn>
2023-12-23 21:57:13 +0800

    add reformat

--- examples/ADA-Agent/utils/utils.py
+++ examples/ADA-Agent/utils/utils.py
@@ -15,3 +15,17 @@ def read_dicts_from_file(file_name):
             dictionary = json.loads(line.rstrip('\n'))
             dict_list.append(dictionary)
     return dict_list
+
+def write_dicts_to_file(dict_list, file_name):
+    """
+    Write a list of dictionaries to a file, with each dictionary on a new line.
+
+    :param dict_list: List of dictionaries to write to the file.
+    :param file_name: Name of the file to write to.
+    """
+    with open(file_name, 'w') as file:
+        for dictionary in dict_list:
+            # Convert the dictionary to a JSON string.
+            dict_str = json.dumps(dictionary)
+            # Write the JSON string to the file followed by a new line character.
+            file.write(dict_str + '\n')
